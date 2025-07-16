3d8c7962d883f065ea4c81a7316af041900a62c1
Luca Giovannesi <l.giovannesi@gmail.com>
2025-07-16 16:21:04 +0200

    env_ok

--- src/lib/env.py
+++ src/lib/env.py
@@ -32,6 +32,8 @@ class NodeEnv(gym.Env):
         self.number_of_high = 0
         self.number_of_low = 0
 
+        self.battery = 0.5
+
         # Solar stuff
         # W = GTI*Surface(m2)*efficiency
         self.solar = solar.Solar(csv_solar_data_path, scale_factor=0.28*0.18)
@@ -67,172 +69,74 @@ class NodeEnv(gym.Env):
 
     def reset(self, *, seed=None, options=None):
         # Reset with random battery percentage, used for validation
-        if options is not None and options["norandom"] == True:
-            self.boot_time_s = 5*60
-            self.device.reset(self.boot_time_s,battery_percentage=0.5)
-        else:
-            self.boot_time_s = rand.randint(5*60, 60*60*24*300)
-            self.device.reset(self.boot_time_s, battery_percentage=rand.randrange(2, 8)/10)
+        #if True or options is not None and options["norandom"] == True:
+        self.boot_time_s = (60*60*7)
+        self.device.reset(self.boot_time_s,battery_percentage=0.5)
+        #else:
+            # self.boot_time_s = rand.randint(5*60, 60*60*24*300)
+            # #self.boot_time_s = self.solar.get_next_sunrise(self.boot_time_s)
+            # self.device.reset(self.boot_time_s, battery_percentage=rand.randrange(2, 8)/10)
         self.time_s = self.boot_time_s
-
         self.device.update(self.time_s)
+
+        self.battery = 0.5
 
         obs = self.__get_obs()
         info = self.__get_info()
         return obs, info
 
     def step(self, action):  # NON VA BENE CHIAMARLO OGNI TOT SECONDI, MA OGNI FINE INFERENZA
-        self.time_s += self.step_s
+        done = False
+        reward = 0.0
 
         # Set solar data
-        solar_current_w = self.solar.get_solar_w(self.time_s)
+        solar_current_w = self.solar.get_solar_w(self.time_s)/40
         self.device.set_pv_production_current_w(solar_current_w)
+        image_consumption = 0.1
+        energy_consumption = -image_consumption
 
-        # Apply action
-        if type(action)==list and len(action)==1:
-            action = action[0]
-        self.device.set_processing_rate(action)
-
-        # Update internal device data
-        self.device.update(self.time_s)
-
-        # Terminate if battery is discharged or chaged at 100%
-        # or (self.stop_on_full_battery and self.device.get_battery_percentage() == 1)
-        terminated = self.device.get_battery_percentage() == 0 
-        if terminated and False:
-            print(f"Battery discarged at time {self.time_s} s -> {self.get_human_uptime()}")
-        # terminated |= self.time_s > 60*60*24*3
-
-        # Get action effects
-        # observation = self.__get_obs()
-
-        # Compute reward
-        # normalized_energy_consumption_w = self.device.get_energy_consumption_w()/(self.device.base_load_energy_w+self.device.full_load_energy_w)
-        # OBJ: Incentiva processamento e disincentiva batteria scarica
-
-        # reward = -abs(0.5-self.device.get_battery_percentage())*normalized_energy_consumption_w
-        # NO! Quanto tutto è carico, la prima parte da rw=-0.5, la seconda parte dovrebbe incentivare a processare, invece andrà a 0 e quindi non processa
-
-        # reward = self.device.get_battery_percentage()*normalized_energy_consumption_w
-        # NO! perchè quando la batteria è scarica, batt=0.1, se processata ottiene qualcosa <0.1, se non processa ottiene 0 e quindi scarica la batteria
-
-        # Processed images
-        if self.get_uptime_s() > 0:
-            # Non sarà mai uguale a 1 perchè passo del tempo a non processare
-            processed_images = self.get_amount_processed_images(
-            ) / (self.get_uptime_s()/self.device.task_duration_s)
+        self.battery = min(1.0, self.battery + solar_current_w)
+        if action == 1:
+            if self.battery > image_consumption:
+                self.battery -= image_consumption
+                reward = 1
+            else:
+                self.battery -= image_consumption
+                reward = -1  # 
+                done = True
+            energy_consumption = image_consumption
         else:
-            processed_images = 0
-        
-        # Energy
-        energy_norm = self.device.get_energy_consumption_w(
-        )/(self.device.base_load_energy_w + self.device.full_load_energy_w)
-
-        battery_level = self.device.get_battery_percentage()  # ∈ [0, 1]
-        #reward = 1 - abs(energy_norm - battery_level) #Se batt_level scende sotto lo 0.5, sono incentivato a non fare niente
-        
-        #Reward 2
-        # reward = 0 #Only second part
-        #if energy_norm > 0.9: #energy norm € (Base_Load, FullLoad)
-        #    reward += self.incentive_factor  # spinta a consumare
-        #wh_for_next_step = self.device.base_load_energy_w * s2h(2*self.step_s)
-        #if self.device.battery_current_capacity_wh < wh_for_next_step: #?Qual'è il valore ottimo?
-        #    reward -= 1  # forte penalità se scarica troppo
-        if self.device.processing_rate > 0:
-            reward = 1
-        else:
-            reward = 0
-        # Reward 3 MALE 169 immagini
-        # wh_for_next_step = self.device.base_load_energy_w * s2h(self.incentive_factor*self.step_s)
-        # min_batt_level = wh_for_next_step / self.device.battery_max_capacity_wh
-        # b = 1 if battery_level > min_batt_level else battery_level
-        # reward = 1-abs(energy_norm - b)
-
-        #reward 4
-        # if self.device.battery_current_capacity_wh < wh_for_next_step:
-        #     reward =-1
-        # else:
-        #     reward = energy_norm
-
-        #reward = -energy_norm
-        # Reward 3
-        # battery_drop = self.prev_battery - battery_level
-        # self.prev_battery = battery_level
-        # reward = battery_drop
-        # reward -= 0.5 if battery_level < 0.1 else 0
-
-        # Reward 4 NON FUNZIONA, tipo 176 immagini
-        # if self.device.get_pv_production_normalized() > 0: #Day
-        #     reward = energy_norm #Molto piccolo
-        # else:  # Night
-        #     reward = -battery_level
-
-        #incentive_image_processing = self.incentive_factor*processed_images
-        #reward = reward+incentive_image_processing
-
-        # Go to the future
-        #self.time_s += self.step_s
-
-
-
-        """                
-        if self.last_day_state == None:
-            self.last_day_state = "day" if self.device.is_day else "night"
-        if self.last_day_state == "day" and self.device.is_day == False:
-            # Switch to night
-            self.last_day_state = "night"
-            print("Night")
-            terminated = True
-        elif self.last_day_state == "night" and self.device.is_day:
-            self.last_day_state = "day"
-            print("day")
-            terminated = False
-         """
-
+            reward = -0.32  # Encourage productivity
+    
+        self.battery -= image_consumption/100
         # Truncate when no more data
-        truncated = solar_current_w < 0
+        #truncated = solar_current_w < 0
 
         # Truncate after 30 days
-        truncated |= self.get_uptime_s() >= 60*60*24*self.truncate_after_d or (self.episode_end_on_sunrise and self.device.is_sunrise(self.time_s))
+        self.time_s += self.step_s
+        done |= self.get_uptime_s() >= (self.step_s*4*576)#self.truncate_after_d or (self.episode_end_on_sunrise and self.device.is_sunrise(self.time_s))
 
-        info = self.__get_info()
-        info["TimeLimit.truncated"] = truncated and not terminated
+        #terminated |= self.time_s > 1000
 
-        return self.__get_obs(), reward, terminated, truncated, info
+        return self.__get_obs(), reward, done, False, {"recharge":solar_current_w, "cons":energy_consumption}
 
     def render(self, mode="human"):
-        print(
-            f"At time {self.time_s} seconds, battery level: {self.device.get_battery_percentage()}%")
+        pass
 
     def __get_obs(self):
-        if self.discrete_state:
-            battery_state = math.floor(
-                10*self.device.get_battery_percentage()/5)  # 0-3
-            return np.array([battery_state])
-        else:
-            # panel_production_a = self.solar.get_solar_w(self.time_s+60*30)/12
-            # future_panel_production_normalized = panel_production_a/2.31
-            # x1, y1 = self.time_s, self.device.get_pv_production_normalized()
-            # x2, y2 = self.time_s+60*60, future_panel_production_normalized
-            # angle_rad = math.atan2(y2-y1, x2-x1)
-            # angle_deg = math.degrees(angle_rad)
-
-            return np.array(
-                [
-                    #self.device.get_pv_production_normalized(),
-                    # self.device.get_energy_consumption_w()/(self.device.base_load_energy_w + self.device.full_load_energy_w),
-                    self.device.get_battery_percentage(),
-                    #((self.time_s/3600) % 24) / 24.0,  # Seconds in a day
-                    # future_panel_production_normalized,
-                    # self.solar.get_solar_w(self.time_s-60*30)/12/2.31
-                    # angle_deg,
-
-                    # self.device.last_wasted_energy_mah/self.device.last_harvested_energy_nah if self.device.last_harvested_energy_nah>0 else 0
-                    # self.get_angular_coefficient(steps_s=30*60) #Between 30 minutes
-                    # self.device.processing_rate,
-                ],
-                dtype=np.float32,
-            )
+        return np.array(
+            [
+                # self.device.get_pv_production_normalized(),
+                # self.device.get_energy_consumption_w()/(self.device.base_load_energy_w + self.device.full_load_energy_w),
+                #self.device.get_battery_percentage(),
+                self.battery,
+                #((self.time_s/3600) % 24) / 24.0,  # Seconds in a day
+                # future_panel_production_normalized,
+                # self.solar.get_solar_w(self.time_s-60*30)/12/2.31
+                # angle_deg,
+            ],
+            dtype=np.float32,
+        )
 
     def __get_info(self):
         return {
